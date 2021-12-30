293b7ff9553f8c1f1ef26867cf256a7c11e77928
Samim Mirhosseini <ssmirr@users.noreply.github.com>
2021-12-30 17:27:35 -0500

    adding typed for tagline

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import Typed from 'react-typed';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -11,18 +12,40 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
-        <img className="hero__title" width="35%" style={{opacity: '70%'}} src='./img/logo.svg'></img>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Get started in 2 min! ⏱️
-          </Link>
-        </div>
+        <div className='row'>
+          <div className='col col-4'>
 
-        <iframe scrolling='no' frameBorder={'no'} allowtransparency='true' allowFullScreen={true} style={{width: '70%', height:'600px'}}  src='https://asciinema.org/a/459098/iframe?autoplay=1&loop=1&preload=1'> </iframe>
+            <h1 className="hero__title">
+              <img width="60%" style={{ opacity: '70%' }} src='./img/logo.svg'></img>
+            </h1>
+
+            <h2 className='hero__subtitle'>
+              <Typed
+                strings={['>_ ' + siteConfig.tagline]}
+                typeSpeed={75}
+              />
+
+            </h2>
+            {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--lg"
+                to="/docs/intro">
+                Get started in 2 min! ⏱️
+              </Link>
+            </div>
+          </div>
+          <div className='col col-8'>
+            <iframe scrolling='no' frameBorder={'no'} allowtransparency='true' allowFullScreen={true} style={{ width: '70%', height: '600px' }} src='https://asciinema.org/a/459098/iframe?autoplay=1&loop=1&preload=1'> </iframe>
+
+          </div>
+
+        </div>
+        <div className='container'>
+
+
+
+        </div>
         
       </div>
     </header>
